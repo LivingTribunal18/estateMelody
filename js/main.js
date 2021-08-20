aab457bc796f9aa83b1cb9412ea5959ced24a51e
karineB <karinebondarenko03@gmail.com>
2021-08-20 14:08:09 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
   let currentFloor = 2;
 
+  // function opens and closes modal window
+  function toggleModal() {
+    $(".modal").toggleClass("is-open");
+  }
+
   // add active class function
   function colorFloor(num) {
     $(".home_image path").removeClass("current-floor");
@@ -9,6 +14,8 @@ $(document).ready(function () {
     currentFloor = num;
   }
 
+  function colorFlat(index, className) {}
+
   // function formatting floor number to ~ '0[2]'
   function formatNum(floorNum) {
     return floorNum.toLocaleString("en-US", {
@@ -16,6 +23,36 @@ $(document).ready(function () {
       useGroupping: false,
     });
   }
+
+  // show modal window
+  $(".home_image").on("click", toggleModal);
+  $(".modal-close-button").on("click", toggleModal);
+  $(".view-flats").on("click", toggleModal);
+
+  $(".flats path").on("mouseover", function () {
+    let inputArrFloor = document.querySelectorAll(".flats path");
+
+    Object.keys(inputArrFloor).map((key) => {
+      if (inputArrFloor[key] == this) {
+        $(".flat-link").removeClass("active");
+        $(".flat-link").eq(key).addClass("active");
+      }
+    });
+  });
+
+  $(".flat-link").on("mouseover", function (e) {
+    e.preventDefault();
+    $(".flat-link").removeClass("active");
+    $(this).toggleClass("active");
+
+    let inputArrLink = document.querySelectorAll(".flat-link");
+    Object.keys(inputArrLink).map((key) => {
+      if (inputArrLink[key] == this) {
+        $(".flats path").removeClass("path_active");
+        $(".flats path").eq(key).addClass("path_active");
+      }
+    });
+  });
 
   // add active class on path hover
   $(".home_image path").on("mouseover", function () {
